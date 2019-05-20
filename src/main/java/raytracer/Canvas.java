1681e1b6a74c07c2ccf2ba52e39b7fbbce28675b
Steven Salisbury <steven@salisburyclan.com>
2019-05-19 21:31:53 -0700

    Added PNG file output to Canvas class

--- src/main/java/raytracer/Canvas.java
+++ src/main/java/raytracer/Canvas.java
@@ -1,5 +1,12 @@
 package raytracer;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Canvas {
 
   private int width;
@@ -45,5 +52,40 @@ public class Canvas {
     if (withinRange(x, y)) {
       pixels[indexOf(x, y)] = color;
     }
+  }
+
+  // Returns the ppm value for the given color channel value.
+  private int toChannelByte(double channel) {
+    if (channel <= 0.0) {
+      return 0;
+    }
+    if (channel >= 1.0) {
+      return 255;
+    }
+    return (int) (channel * 256.0);
+  }
+
+  // Writes the contents of this Canvas to the given raster image.
+  private void writeToRaster(WritableRaster raster) {
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        Color c = pixelAt(x, y);
+        int[] colorArray =
+            new int[] {toChannelByte(c.r()), toChannelByte(c.g()), toChannelByte(c.b())};
+        raster.setPixel(x, y, colorArray);
+      }
+    }
+  }
+
+  private RenderedImage toImage() {
+    BufferedImage image = new BufferedImage(width(), height(), BufferedImage.TYPE_INT_RGB);
+    writeToRaster(image.getRaster());
+    return image;
+  }
+
+  // Writes this canvas to the given file in PNG format.
+  public void writePngFile(File outFile) throws IOException {
+    RenderedImage image = toImage();
+    ImageIO.write(image, "png", outFile);
   }
 }
